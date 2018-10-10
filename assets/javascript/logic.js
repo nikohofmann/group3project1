e1b997a338e107dce56e11e077572d2fab3ac25c
nikohofmann <41592016+nikohofmann@users.noreply.github.com>
2018-10-09 23:11:20 -0600

    Added input box with Google Maps API & Autocomplete

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -25,3 +25,46 @@
         console.log("Humidity: " + response.main.humidity);
         console.log("Temperature (F): " + response.main.temp);
       });
+
+
+      var autocomplete;
+
+      function initAutocomplete() {
+        // Create the autocomplete object, restricting the search to geographical
+        // location types.
+        autocomplete = new google.maps.places.Autocomplete(
+            /** @type {!HTMLInputElement} */(document.getElementById('autocomplete')),
+            {types: ['(cities)'],
+            componentRestrictions: {'country': 'us'}
+            });
+
+        // When the user selects an address from the dropdown, populate the address
+        // fields in the form.
+        autocomplete.addListener('place_changed', fillInAddress);
+      }
+
+      function fillInAddress() {
+        // Get the place details from the autocomplete object.
+        var place = autocomplete.getPlace();
+        var placeLat = place.geometry.location.lat();
+        var placeLng = place.geometry.location.lng();
+        console.log(place.formatted_address + " Lat: " + placeLat + " Lng: " + placeLng);
+      }
+
+      // Bias the autocomplete object to the user's geographical location,
+      // as supplied by the browser's 'navigator.geolocation' object.
+      function geolocate() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var geolocation = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+              center: geolocation,
+              radius: position.coords.accuracy
+            });
+            autocomplete.setBounds(circle.getBounds());
+          });
+        }
+      }
